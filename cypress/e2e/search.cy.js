e3138fab6087c8246bea9181fc0a5061e13d1917
arthurH <a.harutyunyan.a@gmail.com>
2024-03-19 23:10:56 +0400

    added nonexisting payment search, improved search test to delete the created item

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -10,7 +10,7 @@ describe("Search functionality suite", () => {
   it("Test Case 1 | Search for an existing payment", function () {
     let paymentDescription = "Planned expense to be paid";
     let searchKeyword = paymentDescription.split(" ")[0];
-    cy.createStandardExpense(paymentDescription);
+    cy.createStandardExpense(paymentDescription).as("newPayment");
     cy.intercept(
       "GET",
       `${
@@ -45,8 +45,40 @@ describe("Search functionality suite", () => {
               });
           });
         }
+        cy.log(this.newPayment);
       });
   });
 
-  it("Test Case 2 | Validate the non existing payment case", () => {});
+  it("Test Case 2 | Validate the non existing payment case", () => {
+    let nonExistingKeyword = "this is a non existing case";
+    cy.intercept(
+      "GET",
+      `${Cypress.env("apis").payments}?filter=0&page=1&limit=100&search=*`
+    ).as("nonExistingSearch");
+    cy.get('input[placeholder="Поиск"]').type(nonExistingKeyword);
+    cy.realPress("Enter");
+    cy.realPress("Enter");
+    cy.wait("@nonExistingSearch")
+      .its("response")
+      .then((res) => {
+        expect(res.body.count).to.equal(0);
+        expect(res.body.results.length).to.equal(0);
+        cy.get("tbody>tr").should("not.exist");
+        cy.get(".empty").should("have.text", "Нет данных");
+      });
+  });
+
+  after(function () {
+    if (this.newPayment) {
+      cy.request({
+        method: "DELETE",
+        url: `${Cypress.env("apis").payments}${this.newPayment}/`,
+        headers: {
+          Authorization: "JWT " + Cypress.env("token"),
+        },
+      }).then((res) => {
+        expect(res.status).to.equal(204);
+      });
+    }
+  });
 });
