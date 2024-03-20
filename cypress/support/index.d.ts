e85bed14196b20ed59f48ec67a3bd7ccb988f464
arthurH <a.harutyunyan.a@gmail.com>
2024-03-20 11:38:38 +0400

    the last commit, fixed the typo in index.d.ts

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -9,10 +9,11 @@ declare namespace Cypress {
     loginViaAPI(username: string, password: string): Chainable<any>;
 
     /**
-     * Use this command to login to application using the API
+     * Use this command to create a payment with
+     * standard category using the API
      * Thie is used to
      * @example
-     * cy.loginViaAPI(username, password)
+     * cy.createStandardExpense(description)
      */
     createStandardExpense(description: string): Chainable<any>;
   }
